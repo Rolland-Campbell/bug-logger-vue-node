08433e93b3f3c29171fa4960f26f2104da70e118
Rolland <Rollandcampbell@msn.com>
2019-12-18 09:47:19 -0700

    schema done

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,10 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    bugs: []
   },
   mutations: {
   },
   actions: {
+    async addBug({ commit, dispatch }, bug) {
+
+    }
   },
   modules: {
   }
